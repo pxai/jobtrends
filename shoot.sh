e57c8569eb0756b900857d8e5ff368292fb0cb90
pxai <pello_altadill@cuatrovientos.org>
2016-08-18 20:44:45 +0200

    ok, added bash script

---
+++ shoot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+START=$1 || 1
+END=$2 || 1680
+
+echo Starting from $START to $END
+
+for (( i=$START; i<=$END; i++ ))
+do 
+    node src/scripts/selenium/promiseMongo.js ${i}; 
+done
+
+# Note: this kind of range does not work in bash
+#for i in {`echo $1`..`echo $2`}
