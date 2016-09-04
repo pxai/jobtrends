554f0b1ed2c07676472b7682fb595727e1c80cd6
Your Name <you@example.com>
2016-09-04 22:25:53 +0200

    Proxy grabbing ok

--- src/scripts/xrayGrabProxies.js
+++ src/scripts/xrayGrabProxies.js
@@ -1,7 +1,7 @@
 var Xray = require('x-ray');
 var x = Xray();
 var mongoose = require('mongoose');
-var proxies = require('../models/offer');
+var proxies = require('../models/proxy');
 var Proxy = mongoose.model('Proxy');
 var exports = module.exports = {};
 
@@ -15,7 +15,7 @@ x('http://google.com', 'title')(function(err, title) {
 */
 var total = 0;
 var page = 1;
-var urlSite = 'http://www.xroxy.com/proxylist.php?port=&type=Anonymous&ssl=&country=&latency=&reliability=&sort=type&desc=&pnum=';
+var urlSite = 'http://www.xroxy.com/proxylist.php?&pnum=';
 
 /**
  * scraps one Page and get 20 items
@@ -25,21 +25,29 @@ exports.scrapPage = function (page) {
      // https://developers.google.com/web/fundamentals/primers/promises/?hl=en
      // Followet XMLHttpRequest
   return new Promise(function(resolve, reject) {
-        x(urlSite + page, {
-            items: x('h2.job-list-title', [{
-            a: 'h2.job-list-title a@href',
-            job_title: 'h2.job-list-title a@title'
-        }])
+        x(urlSite + page, 'body',{
+            rows0: x('tr.row0', ['td']),
+            rows1: x('tr.row1', ['td'])
+           // port: 'h2.job-list-title a@title'
         })(function(err, obj) {
             console.log('Page: ' + page);
-                          console.log(obj);
-            var offers = [];
-            if (obj != undefined && obj.items != undefined) {
-                for (var i = 0; i < obj.items.length;i++) {
-                    if (obj.items[i].a.indexOf(urlSite) == -1) {
-                        console.log('db.offer.insert({url:\''+obj.items[i].a + '\', title:\'' + obj.items[i].job_title+'\'})');
-                        offers.push(obj.items[i]);
-                    }
+
+            var all = obj.rows0.concat(obj.rows1);
+             console.log(all);
+            var proxies = [];
+            if (obj.rows0 != undefined && obj.rows1 != undefined) {
+                for (var i = 0; i < all.length;i+=6) {
+
+                    var proxyObject = {};
+                    proxyObject.ip = all[++i].replace(/[\n]*$/g,'');
+                    proxyObject.port = all[++i];
+                    proxyObject.type = all[++i];  
+                    console.log(' Saving proxy: ');
+                    console.log(proxyObject);    
+
+                    if (proxyObject.ip != '') {
+                         proxies.push(proxyObject);
+                    }              
                 }
             } else {
               console.log('Obj void');
@@ -47,10 +55,10 @@ exports.scrapPage = function (page) {
             }
 
             // Resolve promise
-            if (offers.length > 0) {
-                resolve(offers);
+            if (proxies.length > 0) {
+                resolve(proxies);
             } else {
-                reject(Error('Offers not found in ' + urlSite + page));
+                reject(Error('Proxies not found in ' + urlSite + page));
             }
         });
 
@@ -60,27 +68,27 @@ exports.scrapPage = function (page) {
 /**
  * saves an offer
  */
-exports.saveOffer = function  (offerObject) {
+exports.saveProxy = function  (proxyObject) {
 
      return new Promise(function(resolve, reject) {
 
-                var offer = new Offer({
-                            url:  offerObject.a,
-                            title:  offerObject.job_title,
-                            body: '',
-                            date: Date.now()
+                var proxy = new Proxy({
+                            ip:  proxyObject.ip,
+                            port:  proxyObject.port,
+                            type: proxyObject.type,
+                            works: true
                         });
 
-                console.log(offerObject);
+                console.log(proxyObject);
 
                     //    offer.save(function (err, offer) {
-               Offer.findOneAndUpdate({url:offerObject.a},offerObject,{upsert: true, 'new': true},function (err, offer) {
+               Proxy.findOneAndUpdate({ip:proxyObject.ip},proxyObject,{upsert: true, 'new': true},function (err, proxy) {
                // offer.save(function (err, offer) {
                     if (err) {
-                        reject(Error('Offers not found in ' + page));
+                        reject(Error('proxies not found in ' + page));
                     } else {
-                        console.log('Saving: ' + offer);
-                        resolve(offer);
+                        console.log('Saving: ' + proxy);
+                        resolve(proxy);
                     }
                 });
 
@@ -101,20 +109,20 @@ exports.inc = function (num) {
 }
 
 
-function saveOffers (offers) {
+function saveProxies (proxies) {
    // promise.all is a builtin function
-   console.log('Lets save offers ' + offers.length);
-  return Promise.all(offers.map(exports.saveOffer));
+   console.log('Lets save proxies ' + proxies.length);
+  return Promise.all(proxies.map(exports.saveProxy));
 }
-var len = 20;
-var n = 0;
+var len = 110;
+var n = -1;
 
-exports.inc(0).then(function repeat(n) {
+exports.inc(-1).then(function repeat(n) {
    console.log('Ok, number is: ' + n);
     if (n < len) {
         return exports.inc(n).then(exports.scrapPage(n).then(
-                function (offers) {
-                    return saveOffers(offers);
+                function (proxies) {
+                    return saveProxies(proxies);
                 }
         )).then(repeat);
     }
